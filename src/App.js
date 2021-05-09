f963ee11ea8eb33fe97a4fc0efd13a61bad9c20e
Lazizbek <lazizbektojiboyev@gmail.com>
2021-05-09 15:01:52 +0500

    added image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+// const memory = './images/memory.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" algin="center">Memories</Typography>
+                <img src='./images/memory.png' alt="memories" height="60px"/>
+            </AppBar>
+        </Container>
     )
 }
 
