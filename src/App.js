d5205246907acc1cf6655a61bfaa252e0760b24d
Lazizbek <lazizbektojiboyev@gmail.com>
2021-05-10 16:33:49 +0500

    from Create to CRUD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-// const memory = './images/memory.png';
+import { getPosts } from './actions/posts';
+
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import memory from './images/memories.png';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" algin="center">Memories</Typography>
-                <img src='./images/memory.png' alt="memories" height="60px"/>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" algin="center">Memories</Typography>
+                <img className={classes.image} src={memory} alt="memories" height="60px"/>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
---
+++ src/actions/posts.js
@@ -0,0 +1,26 @@
+import * as api from '../api';
+
+export const getPosts = () => async (dispatch) => {
+
+    try{
+        const { data } = await api.fetchPosts();
+
+        dispatch({ type: 'FETCH_ALL', payload: data })
+    } catch (e) {
+        console.log(e.message)
+    }
+
+    const action = { type: 'FETCH_ALL', payload: [] }
+
+    dispatch(action)
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+      const { data } = await api.createPost(post);
+  
+      dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
