029ace2051409fe0d3dce76c16f84f8d57726a9e
Lazizbek <lazizbektojiboyev@gmail.com>
2021-05-12 10:48:49 +0500

    thats all :)

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
-
 import App from './App'; 
 import './index.css';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
